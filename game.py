9b6d34cc1a0e288ae17d5aeb16adf10e348e1602
Steffen Lesch <steffen.lesch96@gmail.com>
2022-08-29 19:20:10 +0200

    refactor game architecture to mvv pattern

--- game.py
+++ game.py
@@ -1,9 +1,18 @@
 from enum import Enum
 from functools import reduce
+from typing import List
 from sudoku import Sudoku
 from cursor import SudokuCursor
 from sudoku import Cell
+from generator import fill, holes
+import curses as cu
+from interval import Interval
 
+def notify_listeners(f):
+    def wrapper(*args):
+        f(*args)
+        args[0].notify()
+    return wrapper
 
 class CursorDirection(Enum):
     UP = 0
@@ -11,29 +20,48 @@ class CursorDirection(Enum):
     DOWN = 2
     LEFT = 3
 
-class Game:
 
-    NUMBER_KEYS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+class Screen():
 
-    def __init__(self, sudoku: Sudoku, difficulty) -> None:
-        self.sudoku = sudoku
-        self.cursor = SudokuCursor(self.sudoku)
-        self.difficulty = difficulty
+    BLINK_DURATION = 0.25
 
-    def sudoku_string(self, cursor = None):
-        sudoku_size = self.sudoku.size()
-        cell_size = self.sudoku.cell_size
+    def __init__(self, window: cu.window):
+        self.window = window
+        self.game: Game = None
+        self.interval = None
+
+    
+    def render(self, cursor = None):
+        self.window.erase()
+        self.window.addstr(self.render_sudoku(cursor))
+        self.window.addstr(self.__available_string(self.game.available()))
+        self.window.addstr(self.__difficulty_string(str(self.game.difficulty)))
+        self.window.addstr(self.__options)
+        
+    __options = "Controls:\t(n)ew; (q)uit; (c)lear;\n\t\t[0-9]: set cell; [arrows]: move; [+/-]: adjust difficulty"
+
+    def __available_string(self, avail):
+        return f"Available:\t{avail} \n"
+
+    def __difficulty_string(self, diff):
+        return f"Difficulty: \t{float(diff) * 100}% holes \n"
+
+    
+    def render_sudoku(self, cursor = None):
+        sudoku = self.game.sudoku
+        sudoku_size = sudoku.size()
+        cell_size = sudoku.cell_size
 
         res = ""
         res += "- " * (sudoku_size + cell_size + 1) + "\n"
 
         for row in range(0, sudoku_size):
             for col in range(0, sudoku_size):
-                cell = str(self.sudoku.get(row, col).value)
+                cell = str(sudoku.get(row, col).value)
 
                 cell = "?" if cell == "0" else cell
 
-                if row == self.cursor.row and col == self.cursor.col:
+                if row == self.game.cursor.row and col == self.game.cursor.col:
                     cell = cursor if cursor != None else cell
 
                 if col == 0:
@@ -51,6 +79,46 @@ class Game:
                 res += "- " * (sudoku_size + cell_size + 1) + "\n"
         return res
 
+
+    def onUpdate(self, game):
+        self.game = game
+        self.restart()
+
+    def start(self):
+
+        self.render(" ")
+        self.interval = Interval(
+            self.BLINK_DURATION, 
+            lambda : self.render(" "),
+            lambda : self.render()
+        )
+        self.interval.start()
+
+    def stop(self):
+        if self.interval:
+            self.interval.stop()
+    
+    def restart(self):
+        self.stop()
+        self.start()
+
+class Game:
+
+    NUMBER_KEYS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    def __init__(self, sudoku: Sudoku, difficulty) -> None:
+        self.sudoku = sudoku
+        self.cursor = SudokuCursor(self.sudoku)
+        self.difficulty = difficulty
+        self.listeners: List[Screen] = []
+    
+    def addListener(self, screen: Screen):
+        self.listeners.append(screen)
+
+    def new(size, difficulty): 
+        return Game(Game.__new_sudoku(size, difficulty), difficulty)
+
+    @notify_listeners
     def move(self, direction: CursorDirection): 
         if (direction == CursorDirection.UP):
             self.cursor.up()
@@ -60,7 +128,8 @@ class Game:
             self.cursor.down()
         elif(direction == CursorDirection.LEFT):
             self.cursor.left()
-    
+
+    @notify_listeners
     def set(self, key): 
         key = int(key)
         try:
@@ -68,14 +137,45 @@ class Game:
         except:
             pass
     
+    @notify_listeners
     def unset(self):
         try:
             self.sudoku.set(self.cursor.row, self.cursor.col, Cell(0, False))
         except:
             pass
+    
+    @notify_listeners
+    def raise_difficulty_by(self, value): 
+        new_difficulty = (self.difficulty * 10) + value
+        if new_difficulty >= 10.0 or new_difficulty <= 0.0: return
+        self.difficulty = new_difficulty / 10 
+        self.sudoku = Game.__new_sudoku(self.sudoku.size(), self.difficulty)
+    
+    @notify_listeners
+    def reset(self):
+        self.sudoku = Game.__new_sudoku(self.sudoku.size(), self.difficulty)
 
     def check_win(self):
         return reduce(lambda prev, cur: prev and cur.solved, self.sudoku.sudoku, Cell(0, True))
 
     def available(self):
         return str(self.sudoku.available(self.cursor.row, self.cursor.col))
+
+    def __new_sudoku(size, difficulty):
+        h = Game.__curry(Game.__flip(holes))(difficulty)
+        s = Game.__compose(h, fill)(Sudoku(size))
+        return s
+
+    def notify(self):
+        for listener in self.listeners:
+            listener.onUpdate(self)
+
+    def __compose(f, g):
+        return lambda x : f(g(x))
+
+    def __flip(f):
+        return lambda a, b: f(b, a)
+    
+    def __curry(f):
+        return lambda a: lambda b: f(a, b)
+
--- main.py
+++ main.py
@@ -1,54 +1,22 @@
-from argparse import ArgumentParser
-from generator import fill, holes
-from sudoku import Sudoku
 import curses as cu
-from interval import Interval
-from game import CursorDirection, Game
+from game import CursorDirection, Game, Screen
 
 options = "Controls:\t(n)ew; (q)uit; (c)lear;\n\t\t[0-9]: set cell; [arrows]: move; [+/-]: adjust difficulty"
-
-def new_game(size, difficulty):
-    sudoku = Sudoku(size)
-    sudoku = fill(sudoku)
-    sudoku = holes(sudoku, difficulty)
-    game = Game(sudoku, difficulty)
-    return game
-
-def available_string(avail):
-    return f"Available:\t{avail} \n"
-
-def difficulty_string(diff):
-    return f"Difficulty: \t{float(diff) * 100}% holes \n"
-
-def render(game: Game, stdscr: cu.window, cursor = None):
-    stdscr.erase()
-    stdscr.addstr(game.sudoku_string(cursor))
-    stdscr.addstr(available_string(game.available()))
-    stdscr.addstr(difficulty_string(str(game.difficulty)))
-    stdscr.addstr(options)
 
 def main(stdscr: cu.window):
 
     size = 9
     difficulty = 0.5
 
-    game = new_game(size, difficulty)
+    game = Game.new(size, difficulty)
+    screen = Screen(stdscr)
+    game.addListener(screen)
+    game.notify()
 
     cu.curs_set(0)
     
     stdscr.nodelay(1)
-    render(game, stdscr, " ")
-
-    def wc():
-        render(game, stdscr, " ")
-
-    def nc():
-        render(game, stdscr)
-
-
-    iv = Interval(0.25, wc, nc)
-    iv.start()
-
+    
     while True:
         try:
             key = stdscr.getkey()
@@ -69,20 +37,16 @@ def main(stdscr: cu.window):
                 break
 
             elif (key.lower() == "n"):
-                game = new_game(size, game.difficulty)
-            
+                game.reset()
+
             elif (key.lower() == "c"):
                 game.unset()
 
             elif (key == "+"):
-                new_difficulty = (game.difficulty * 10) + 1
-                if (new_difficulty >= 10.0): continue
-                game = new_game(size, new_difficulty / 10)
+                game.raise_difficulty_by(1)
 
             elif (key == "-"):
-                new_difficulty = (game.difficulty * 10) - 1
-                if (new_difficulty == 0.0): continue
-                game = new_game(size, new_difficulty/10)
+                game.raise_difficulty_by(-1)
 
             elif (key in game.NUMBER_KEYS):
                 game.set(key)
@@ -92,7 +56,7 @@ def main(stdscr: cu.window):
         except:
             pass
     
-    iv.stop()
+    screen.stop()
     cu.endwin()
 
 
